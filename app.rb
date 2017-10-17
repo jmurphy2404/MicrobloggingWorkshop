6a26c370ff5c72ada66c6474f532f19ab3aec62b
Sophie Bushman <Sophieebushman@gmail.com>
2017-10-17 15:46:22 -0400

    updated files

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sqlite3'
+require './models'
+require 'sinatra/flash'
+
+set :database, {adapter: 'sqlite3', database: 'micro.sqlite3'}
+
+before do
+	current_user
+end
+
+#login protection
+before ['/questions/new','/questions'] do
+	redirect '/' unless @current_user
+end
+
+get '/' do
+	erb :home
+end
